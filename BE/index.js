b0e9a9e5337eef2282ff2585bfaae17151b90e36
Manish6379 <b.manish246810@gmail.com>
2024-11-05 16:41:07 +0530

    backend setup

--- BE/index.js
+++ BE/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/fruit', (req, res)=>{
-    res.send({msg:"Hi, This is Fruit"})
+    res.send({msg:"Hi, This is Fruit "})
 });
 
 app.listen(5000, () => console.log("server start at 5000"));
